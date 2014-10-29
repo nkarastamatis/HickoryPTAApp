ff3166f962f688be7f9b14de2d62e5aee9e6aad6
Nick Karastamatis <nkarastamatis@gmail.com>
2014-10-29 00:15:29 -0400

    Startup Migrations

--- HickoryPTAApp/Global.asax.cs
+++ HickoryPTAApp/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity.Migrations;
 
 namespace HickoryPTAApp
 {
@@ -19,6 +20,14 @@ namespace HickoryPTAApp
 
             ModelBinders.Binders[typeof(DateTime)] =
                 new PTAData.DateAndTimeModelBinder() { Date = "Date", Time = "Time" };
+
+            var applicationContextConfiguration = new HickoryPTAApp.Migrations.ApplicationDbContextMigrations.Configuration();
+            var migrator = new DbMigrator(applicationContextConfiguration);
+            migrator.Update();
+
+            var hickoryPtaAppContextConfiguration = new HickoryPTAApp.Migrations.HickoryPTAAppContextMigrations.Configuration();
+            migrator = new DbMigrator(hickoryPtaAppContextConfiguration);
+            migrator.Update();
         }
     }
 }
